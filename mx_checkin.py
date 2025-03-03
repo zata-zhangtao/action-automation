fcec42f6a2ce88cf5ecde0054f169ca5af16dc8b
zata-zhangtao <57862134+zata-zhangtao@users.noreply.github.com>
2025-03-03 13:33:51 +0800

    Update mx_checkin.py

--- mx_checkin.py
+++ mx_checkin.py
@@ -4,6 +4,7 @@ import time
 from datetime import datetime
 import os
 Cookie = os.environ.get("MX_COOKIE")  # 把Cookie放置在action的环境变量中
+print(Cookie)
 def checkin():
     # 替换为实际的签到接口 URL
     url = "https://mxwljsq.top/user/checkin"
